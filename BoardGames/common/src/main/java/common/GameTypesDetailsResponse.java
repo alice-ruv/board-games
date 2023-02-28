9c735683fa4b303ecd27755729ed4a423835b6e9
alice-ruv <124344785+alice-ruv@users.noreply.github.com>
2023-02-28 16:12:37 +0200

    Update GameTypesDetailsResponse.java

--- BoardGames/common/src/main/java/common/GameTypesDetailsResponse.java
+++ BoardGames/common/src/main/java/common/GameTypesDetailsResponse.java
@@ -8,16 +8,20 @@ public class GameTypesDetailsResponse
     @JsonbProperty(value = "gameTypeDetailsList")
     private List<GameTypeDetails> gameTypeDetailsList;
 
-    public List<GameTypeDetails> getGameTypeDetailsList() {
+    public List<GameTypeDetails> getGameTypeDetailsList() 
+    {
         return gameTypeDetailsList;
     }
+    
     @SuppressWarnings("unused") //used by json convertor
     public void setGameTypeDetailsList(List<GameTypeDetails> gameTypeDetailsList)
     {
         this.gameTypeDetailsList = gameTypeDetailsList;
     }
+    
     @SuppressWarnings("unused") //used by json convertor
     public GameTypesDetailsResponse() {}
+    
     public GameTypesDetailsResponse(List<GameTypeDetails> details)
     {
         gameTypeDetailsList = details;
