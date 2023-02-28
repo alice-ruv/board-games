3e6e48ac6dc18922cb6f9207eee0f2109e48872c
alice-ruv <124344785+alice-ruv@users.noreply.github.com>
2023-02-28 15:48:00 +0200

    Update GameStatus.java

--- BoardGames/common/src/main/java/common/GameStatus.java
+++ BoardGames/common/src/main/java/common/GameStatus.java
@@ -4,6 +4,6 @@ public enum GameStatus
     WAIT_FOR_ALL_PLAYERS,
     READY_TO_START,   //two players performed join to game
     RUNNING,
-    FINISHING, //when player is quiting (disconnected) the game in the middle or canceling before the other player joined
+    FINISHING, //player is quiting (disconnected) the game in the middle or canceling before the other player joined
     FINISHED
 };
