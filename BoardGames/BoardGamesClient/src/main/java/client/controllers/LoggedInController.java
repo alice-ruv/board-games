2b7a7c6ddf177db5e9887a5ebed747a0e9765a5a
alice-ruv <124344785+alice-ruv@users.noreply.github.com>
2023-05-19 19:03:16 +0300

    Update LoggedInController.java

--- BoardGames/BoardGamesClient/src/main/java/client/controllers/LoggedInController.java
+++ BoardGames/BoardGamesClient/src/main/java/client/controllers/LoggedInController.java
@@ -9,7 +9,6 @@ import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.RadioButton;
 import javafx.scene.control.ToggleGroup;
-
 import java.net.URL;
 import java.util.ResourceBundle;
 import static common.GameTypes.BATTLESHIP;
