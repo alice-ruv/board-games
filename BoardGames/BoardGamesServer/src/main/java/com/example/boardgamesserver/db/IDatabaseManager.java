d552c71e03945e6e1e21b5193428c146fe253695
alice-ruv <124344785+alice-ruv@users.noreply.github.com>
2023-05-20 14:58:06 +0300

    Update IDatabaseManager.java

--- BoardGames/BoardGamesServer/src/main/java/com/example/boardgamesserver/db/IDatabaseManager.java
+++ BoardGames/BoardGamesServer/src/main/java/com/example/boardgamesserver/db/IDatabaseManager.java
@@ -4,12 +4,13 @@ import common.*;
 import common.exceptions.UserAlreadyExistException;
 import common.exceptions.GeneralErrorException;
 import common.exceptions.UserNotFoundException;
-
 import java.util.List;
 
-public interface IDatabaseManager {
+public interface IDatabaseManager 
+{
     User createUser(String username, String password, String displayName) throws UserAlreadyExistException, GeneralErrorException;
     User getUser(String username, String password) throws UserNotFoundException, GeneralErrorException;
+    
     int /*gameId*/ joinGame(int userId, int gameTypeId) throws GeneralErrorException;
     List<GameTypeDetails> getGameTypesDetails() throws GeneralErrorException;
     List<GameResult> getUserGameResults(int gameTypeId, int userId) throws GeneralErrorException;
@@ -18,9 +19,6 @@ public interface IDatabaseManager {
     boolean updateGame(GameUpdateData data) throws GeneralErrorException;
     void updatePlayerReady(int userId, int gameId) throws GeneralErrorException;
     GameFullData getGameFullData(int gameId) throws GeneralErrorException;
-
     void deleteGame(int gameId) throws GeneralErrorException;
-
     List<Integer> getAllRunningGames() throws GeneralErrorException;
 }
-
